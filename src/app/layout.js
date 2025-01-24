7bd5803cf1fb72f5ad2ab5b87e4ed87d7e5af6f1
Naveen Saini <naveensaini94667@gmail.com>
2025-01-24 16:35:23 +0530

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,10 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <Ed/>
-
-      </head>
+      
       <body
         className="">
           <main className="min-h-screen bg-black text-white">
@@ -44,6 +41,7 @@ export default function RootLayout({ children }) {
     </main>
     <SEOTags/>
     <Footer />
+    <Ed/>
       </body>
       <GoogleAnalytics />
     </html>
